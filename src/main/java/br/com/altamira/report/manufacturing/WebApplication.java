b6d5e0732a3e3a909763d739abf4911d6546283f
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-11-14 08:45:31 -0200

    add suport to data-model, add support to rest endpoint

---
+++ src/main/java/br/com/altamira/report/manufacturing/WebApplication.java
@@ -0,0 +1,13 @@
+package br.com.altamira.report.manufacturing;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author alessandro.holanda
+ */
+@ApplicationPath("/")
+public class WebApplication extends Application {
+		
+}
