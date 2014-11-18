f4fd00c5b326fea1e10fbfe49f49815becb1098f
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-11-18 15:40:20 +0530

    Done with Ordem de Servico (Almoxarifado) report.

--- src/main/java/br/com/altamira/report/manufacturing/Reports.java
+++ src/main/java/br/com/altamira/report/manufacturing/Reports.java
@@ -214,5 +214,30 @@ public class Reports  extends ReportConfig{
 		return serviceOrderTransportationReport.getReport(id);	
 
 	}
+	
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent
+	 * to the client as "application/pdf" media type.
+	 * @return 
+	 * @return 
+	 *
+	 */
+	@GET @Path("/bom/{id}/warehouse")
+	@Produces("application/pdf") 
+	public  Response serviceOrderWarehouse(
+			@Context HttpServletRequest req, 
+			@Context HttpServletResponse resp, 
+			@PathParam("id") String id) 
+					throws ServletException, IOException {
+
+		//CHECK FOR AUTH TOKEN
+		if (checkAuth(token).getStatus() != 200) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid Token: " + token).build();
+		}
+		
+		WarehouseReport serviceOrderWarehouseReport = new WarehouseReport();
+		return serviceOrderWarehouseReport.getReport(id);	
+
+	}
 
 }
