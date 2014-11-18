9e3dcbb15acfe7cb7480d0bdf6fe5bb3f27c920b
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-11-18 14:46:44 +0530

    Done with the migration of Ordem de Servico (Transporte) report.

--- src/main/java/br/com/altamira/report/manufacturing/Reports.java
+++ src/main/java/br/com/altamira/report/manufacturing/Reports.java
@@ -189,5 +189,30 @@ public class Reports  extends ReportConfig{
 		return serviceOrderShippingReport.getReport(id);	
 
 	}
+	
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent
+	 * to the client as "application/pdf" media type.
+	 * @return 
+	 * @return 
+	 *
+	 */
+	@GET @Path("/bom/{id}/transportation")
+	@Produces("application/pdf") 
+	public  Response serviceOrderTransportation(
+			@Context HttpServletRequest req, 
+			@Context HttpServletResponse resp, 
+			@PathParam("id") String id) 
+					throws ServletException, IOException {
+		
+		//CHECK FOR AUTH TOKEN
+		if (checkAuth(token).getStatus() != 200) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid Token: " + token).build();
+		}
+
+		TransportationReport serviceOrderTransportationReport = new TransportationReport();
+		return serviceOrderTransportationReport.getReport(id);	
+
+	}
 
 }
